de4f34135cfd811e8b8b502360bcb6b0aebcdd2b
PelehAlbino <marciofilho2004@bol.com.br>
2024-09-16 16:14:02 -0300

    Término do CSS de modo geral

--- routes/index.ts
+++ routes/index.ts
@@ -9,6 +9,14 @@ class IndexRoute {
 		res.render("index/index", opcoes);
 	}
 
+	public async produtos(req: app.Request, res: app.Response) {
+		let opcoes = {
+			titulo: "Catalogo"
+		};
+
+		res.render("index/produtos", opcoes);
+	}
+
 	public async sobre(req: app.Request, res: app.Response) {
 		let opcoes = {
 			titulo: "Sobre"
@@ -16,6 +24,21 @@ class IndexRoute {
 
 		res.render("index/sobre", opcoes);
 	}
+	public async camisa(req: app.Request, res: app.Response) {
+		let opcoes = {
+			titulo: "Camisa"
+		};
+
+		res.render("index/camisa", opcoes);
+	}
+	public async blusa(req: app.Request, res: app.Response) {
+		let opcoes = {
+			titulo: "Blusa"
+		};
+
+		res.render("index/blusa", opcoes);
+	}
+
 }
 
 export = IndexRoute;
